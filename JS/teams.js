280765f0bdce61b72ecae756b8736f8b8a38f8ae
edgar vitor <vitoredgar888@gmail.com>
2024-12-17 02:10:28 -0300

    new uptades

--- JS/teams.js
+++ JS/teams.js
@@ -1,7 +1,13 @@
+/*criando arrays manipulaveis*/
+
 const tabMenuTeams = document.querySelectorAll('.js-tab-teams li');
 const tabContent = document.querySelectorAll('.js-content-teams section');
 
-function ativarTab(index){
+/*tornando ativa no site ao carregar*/
+tabContent[0].classList.add('active-content-teams');
+tabMenuTeams[0].classList.add('active-tab-teams');
+
+function ativarTabTeams(index){
 
     tabMenuTeams.forEach((i) => {
 
@@ -10,7 +16,28 @@ function ativarTab(index){
     })
 
     tabMenuTeams[index].classList.add('active-tab-teams');
+}
 
+function ativarContentTeams(index){
 
+    tabContent.forEach(i => {
+        i.classList.remove('active-content-teams');
+    });
+
+    tabContent[index].classList.add('active-content-teams');
 
 }
+
+
+/* adicionar ao clique */
+
+tabMenuTeams.forEach((item,i) => {
+
+    item.addEventListener('click',function(){
+
+        ativarTabTeams(i);
+        ativarContentTeams(i);
+
+    })
+
+})
